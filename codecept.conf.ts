428509a8702d3362eb705c85afec902ca01147c7
aditya42 <dhendeaditya510@gmail.com>
2026-06-28 17:00:29 +0530

    framework commit

---
+++ codecept.conf.ts
@@ -0,0 +1,26 @@
+import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+export const config: CodeceptJS.MainConfig = {
+  tests: './tests/*_test.ts',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'https://getbootstrap.com',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.ts'
+  },
+  noGlobals: true,
+  plugins: {},
+  name: 'NodeProjects',
+  require: ['tsx/cjs']
+}
